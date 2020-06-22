6fe0d6d06d4ed3dbff803c11a46711bbff112286
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2020-06-22 11:16:39 +0200

    Updates

---
+++ precache-manifest.940beb54d5742311d2e7f8ce775ccc59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd17e4040d81296ba76cfeeb57414532",
+    "url": "/react-starter/static/media/logo.dd17e404.svg"
+  },
+  {
+    "revision": "18eedbb68b8803eb68d1",
+    "url": "/react-starter/static/js/runtime~main.18eedbb6.js"
+  },
+  {
+    "revision": "003f904093bf37f8726a",
+    "url": "/react-starter/static/js/main.003f9040.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-starter/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "003f904093bf37f8726a",
+    "url": "/react-starter/static/css/main.3245f820.chunk.css"
+  },
+  {
+    "revision": "49a11dc4bb9c018c9c94832eb34c9dea",
+    "url": "/react-starter/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-starter/precache-manifest.b21a4324f62bdf0103814a565781f4a8.js"
+  "/react-starter/precache-manifest.940beb54d5742311d2e7f8ce775ccc59.js"
 );
 
 workbox.clientsClaim();
